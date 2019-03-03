a3bfecabbf5f245199e99be88f3ad4b5a2f2211f
Andriy Legeta <andriylegeta@gmail.com>
2019-03-03 22:10:56 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AuthorizationModule} from "./modules/authorization.module";
+import {AuthRoutingModule} from "./modules/auth-routing.module";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthorizationModule,
+    AuthRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
